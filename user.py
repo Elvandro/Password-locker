fee4820c4b23abf193b936bae867df7d86b4b1de
Elvis Amuni <elvisamuni22@gmail.com>
2018-06-11 08:10:05 +0300

    This commit will create a new instance of a user

---
+++ user.py
@@ -0,0 +1,13 @@
+class User:
+    """
+    Class that generates new instances of users
+    """
+    user_list = [] #Empty user list
+
+    def __init__(self,user_name, password):
+        self.user_name = user_name
+        self.password = password
+
+
+
+        
